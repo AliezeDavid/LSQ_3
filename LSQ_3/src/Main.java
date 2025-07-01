7379cb958df66a50b38c4d06c2536f6aeb70d055
AliezeDavid <alieze.david@lmu.edu.ng>
2025-07-01 01:23:41 +0100

    Add files via upload

---
+++ LSQ_3/src/Main.java
@@ -0,0 +1,25 @@
+//Now una don dey turn guru for stack sha
+
+//--------------------Question 3 -------------------------
+//Write a Java program to check whether a
+//Stack collection is empty or not.
+//HINT: stck_list2.empty()
+
+import java.util.Stack;
+
+public class Main{
+    public static void main(String[] args) {
+
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Cain");
+        stack.push("Abel");
+
+        System.out.println(stack.empty());
+
+        //Definitely if you follow the story line of what we've been saying
+        // since morning sha you should know this would give you??????????
+
+    }
+}
